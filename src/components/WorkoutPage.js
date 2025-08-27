a152fa06b3402b2da9c8632f3015fcd160808e49
ash13y16 <ashley20854@gmail.com>
2025-08-26 21:53:31 -0500

    WorkoutPage structure done

--- src/components/WorkoutPage.js
+++ src/components/WorkoutPage.js
@@ -1,9 +1,31 @@
 import './styles.css';
+import {useNavigate} from "react-router-dom";
 
 function WorkoutPage() {
+
+    //Button for exiting
+    const navigate = useNavigate();
+
+    const exit = () => {
+        navigate("/");
+    };
+
     return (
         <div className={'WorkoutPage'}>
-            <button className={'exitButton'}>X</button>
+            <button className={'exitButton'} onClick={exit}>
+                x
+            </button>
+            <h1>Exercise Name</h1>
+            <div style={{display: "flex", justifyContent: 'space-between', width: '200px'}}>
+                <h2>Sets</h2>
+                <h2>Reps</h2>
+            </div>
+            <button className={'restTimer'}>
+                Rest
+            </button>
+            <button className={'startButton'} style={{position: "relative", bottom: 0}}>
+                Start
+            </button>
         </div>
     );
 }
